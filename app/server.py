36f8bfbe1ba19c5073aa9f594aa6f985da9b83e8
Swelia Monteiro <sweliamonteiro05@gmail.com>
2021-05-09 03:27:43 +0530

    Update server.py

--- app/server.py
+++ app/server.py
@@ -9,8 +9,8 @@ from starlette.middleware.cors import CORSMiddleware
 from starlette.responses import HTMLResponse, JSONResponse
 from starlette.staticfiles import StaticFiles
 
-export_file_url = 'https://www.dropbox.com/s/6bgq8t6yextloqp/export.pkl?raw=1'
-export_file_name = 'export.pkl'
+export_file_url = 'https://www.dropbox.com/s/dc29ihuuechv6c6/model.pkl?dl=1'
+export_file_name = 'model.pkl'
 
 classes = ['Betul lighthouse', 'Buddhist Temple', 'Mae de Deus Church', 'Mahadev Temple', 'Mallikarjuna Temple', 'Museum House, Torda', 'Reis Magos Fort', 'Shree Shantadurga Temple, Fatorpa', 'St. Cajetan Church', 'Sunaparanta, Goa Arts Centre']
 path = Path(__file__).parent
