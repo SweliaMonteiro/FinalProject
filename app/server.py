395d120ca65c7c0c67fbab3f6fc410203be19a25
Swelia Monteiro <sweliamonteiro05@gmail.com>
2021-05-10 19:52:50 +0530

    Update server.py

--- app/server.py
+++ app/server.py
@@ -60,8 +60,8 @@ async def analyze(request):
     img_data = await request.form()
     img_bytes = await (img_data['file'].read())
     img = open_image(BytesIO(img_bytes))
-    prediction = learn.predict(img)
-    return JSONResponse({'result': str(prediction)})
+    pred,pred_idx,probs = learn.predict(img)
+    return JSONResponse({'result': str(pred)+' '+str(probs[pred_idx]*100)})
 
 
 if __name__ == '__main__':
