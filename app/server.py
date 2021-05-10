96cb4c1d1dd99c6fcb95aafbb5cc7ff934175cc6
Swelia Monteiro <sweliamonteiro05@gmail.com>
2021-05-11 02:32:00 +0530

    Update server.py

--- app/server.py
+++ app/server.py
@@ -9,7 +9,7 @@ from starlette.middleware.cors import CORSMiddleware
 from starlette.responses import HTMLResponse, JSONResponse
 from starlette.staticfiles import StaticFiles
 
-export_file_url = 'https://drive.google.com/uc?export=download&id=1eTdbYvohNuj31Zjtz2RW0Degj3WMHu35'
+export_file_url = 'https://www.dropbox.com/s/dc29ihuuechv6c6/model.pkl?dl=1'
 export_file_name = 'model.pkl'
 
 classes = ['Betul lighthouse', 'Buddhist Temple', 'Mae de Deus Church', 'Mahadev Temple', 'Mallikarjuna Temple', 'Museum House, Torda', 'Reis Magos Fort', 'Shree Shantadurga Temple, Fatorpa', 'St. Cajetan Church', 'Sunaparanta, Goa Arts Centre']
