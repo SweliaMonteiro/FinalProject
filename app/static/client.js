6d9e58c8b8d04847a28dfa9a6c45e42540649e16
Swelia Monteiro <sweliamonteiro05@gmail.com>
2021-05-10 15:47:34 +0530

    Update client.js

--- app/static/client.js
+++ app/static/client.js
@@ -29,7 +29,7 @@ function analyze() {
   xhr.onload = function(e) {
     if (this.readyState === 4) {
       var response = JSON.parse(e.target.responseText);
-      el("result-label").innerHTML = `Prediction = ${response["result"]}`;
+      el("result-label").innerHTML = `Prediction = ${response} `;
     }
     el("analyze-button").innerHTML = "Analyze";
   };
