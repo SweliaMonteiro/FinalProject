96d4a69acb635ef4a0e08c76f0f651cf3e61a953
Swelia Monteiro <sweliamonteiro05@gmail.com>
2021-05-10 20:33:18 +0530

    Update client.js

--- app/static/client.js
+++ app/static/client.js
@@ -29,7 +29,7 @@ function analyze() {
   xhr.onload = function(e) {
     if (this.readyState === 4) {
       var response = JSON.parse(e.target.responseText);
-      if(int(${response["prob"][0]}) > 70){
+      if(int(response["prob"][0]) > 70) {
       el("result-label").innerHTML = `Result = ${response["result"]}` ;}
       else{
       el("result-label").innerHTML = `Selected image can't be predicted` ;}
